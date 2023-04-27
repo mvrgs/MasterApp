63d5007fb5cbee08e7bd2db894613a6c218dbe7e
mvrgs <m.vargas@estudiantec.cr>
2023-04-27 00:59:20 -0600

    interfaz de inicio de sesion, master y client

--- src/main/java/com/example/masterapp/Main.java
+++ src/main/java/com/example/masterapp/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 276, 317);
+        stage.setTitle("Inicio de sesion");
         stage.setScene(scene);
         stage.show();
     }
