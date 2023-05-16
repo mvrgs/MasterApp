16a210c59cebf505dcf1bdcd6f22ee064b94f9b8
CalacasChidas <ale-jimenez@estudiantec.cr>
2023-05-15 23:59:56 -0600

    Editar usuarios completo

--- src/main/java/com/example/masterapp/EditarUsuariosController.java
+++ src/main/java/com/example/masterapp/EditarUsuariosController.java
@@ -3,6 +3,11 @@ package com.example.masterapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
+import static com.example.masterapp.Main.in;
+import static com.example.masterapp.Main.out;
+
 public class EditarUsuariosController {
 
     @FXML
@@ -16,10 +21,29 @@ public class EditarUsuariosController {
 
     @FXML
     private TextField usuario;
-    ArbolBinarioBusqueda arbol = ;
-
-    public Usuario user(String nombre){
-
+    Boolean respuesta ;
+    @FXML
+    private void user() throws IOException, ClassNotFoundException {
+        String nombre = findUsuario.getText();
+        out.writeObject("validarUsuario");
+        out.writeObject(nombre);
+        respuesta = (Boolean) in.readObject();
+        System.out.println(respuesta);
+        if (respuesta){
+            usuario.setText((String) in.readObject());
+            contra.setText((String) in.readObject());
+            roll.setText((String) in.readObject());
+        }
+        else{
+            usuario.setText("Usuario no existe");
+        }
+    }
+    @FXML
+    private void edit() throws IOException, ClassNotFoundException{
+        out.writeObject("editUser");
+        out.writeObject(usuario.getText());
+        out.writeObject(contra.getText());
+        out.writeObject(roll.getText());
     }
 }
 
