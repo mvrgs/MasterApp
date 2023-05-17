24ed452555f2d895aaa8981e5ec4a2cec209817a
CalacasChidas <ale-jimenez@estudiantec.cr>
2023-05-16 21:36:47 -0600

    :p

---
+++ src/main/java/com/example/masterapp/EditarPlatillosController.java
@@ -0,0 +1,26 @@
+package com.example.masterapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class EditarPlatillosController {
+
+    @FXML
+    private TextField ID;
+
+    @FXML
+    private TextField calorias;
+
+    @FXML
+    private TextField findPlate;
+
+    @FXML
+    private TextField nombre;
+
+    @FXML
+    private TextField precio;
+
+    @FXML
+    private TextField tiempo;
+
+}
